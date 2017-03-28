556e64b19b4084c073558a9fa53070dc31062d5d
lgarzio <lori.m.garzio@gmail.com>
2017-03-28 15:44:51 -0400

    fixed bugs
    some bug fixes:
    - fixed annotations for streams when there is more than 1 file per
    deployment
    - write additional issues to separate files (if they’re not necessarily
    annotations)
    - if there are data files for collocated instruments, write that
    information to a separate file

--- tools/annotate_streams.py
+++ tools/annotate_streams.py
@@ -16,6 +16,7 @@ import simplejson as json
 import csv
 import os
 from datetime import datetime as dt
+import re
 
 
 def make_dir(save_dir):
@@ -25,58 +26,110 @@ def make_dir(save_dir):
         pass
 
 
-def extract_gaps(data, stream_csv, user):
-    for d in data['deployments']:
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split('(\d+)', text) ]
+
+
+def extract_gaps(data, stream_csv, stream_csv_other, stream_name, user):
+    # read in deployment information from asset management and start and end dates of the data requested by deployment
+    deployment_list = data['deployments']
+    deployment_list_sorted = deployment_list.keys()
+    deployment_list_sorted.sort(key = natural_keys)  # sorts the deployments
+
+    for d in deployment_list_sorted:
         deployment = d
-        deploy_begin = data['deployments'][d]['begin']
-        deploy_end = data['deployments'][d]['end']
+        deploy_begin = data['deployments'][d]['start'] # asset management start date
+        deploy_end = data['deployments'][d]['end'] # asset management end date
+        data_begin = data['deployments'][d]['data_times']['start'] # first data file start date
+#        data_end = data['deployments'][d]['data_times']['end']  # last data file end date
+
+        # read in stream information
         for s in data['deployments'][d]['streams']:
             stream = s
-            for x in data['deployments'][d]['streams'][s]['files']:
-                data_begin = data['deployments'][d]['streams'][s]['files'][x]['data_start']
-                data_end = data['deployments'][d]['streams'][s]['files'][x]['data_end']
-                gaps = data['deployments'][d]['streams'][s]['files'][x]['time_gaps']
+            if stream == stream_name:  # if stream matches the stream from the file name, write to main .csv
+                file = stream_csv
+            else:
+                file = stream_csv_other  # if the stream does not match the stream from the file name, write to the collocated instrument .csv
 
-                if deploy_begin is data_begin:
-                    pass
-                else:
-                    newline = (stream,deployment,deploy_begin,data_begin,'','NOT_AVAILABLE','','check - data begin does not equal deployment begin date',user)
-                    stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+            # test if deployment begin from data equals deployment begin from asset management.
+            if deploy_begin is data_begin:
+                pass
+            else:
+                newline = (stream,deployment,deploy_begin,data_begin,'','NOT_AVAILABLE','','check: data begin does not equal deployment begin date',user)
+                file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
+            cnf = 0 # to count files
+            file_list = data['deployments'][d]['streams'][s]['files'] # list of files for the deployment
+            file_list_sorted = file_list.keys()
+            file_list_sorted.sort(key = natural_keys)  # sorts the data files
 
+            # read data file information
+            for x in file_list_sorted:
+                data_start_file = data['deployments'][d]['streams'][s]['files'][x]['data_start']  # data start date of file
+                data_end_file = data['deployments'][d]['streams'][s]['files'][x]['data_end'] # data end date of file
+                gaps = data['deployments'][d]['streams'][s]['files'][x]['time_gaps']  # list of gaps
+
+                # if cnf is not 0: # check data gaps between files
+                #     if file_end is data_start_file:
+                #         pass
+                #     else:
+                #         newline = (stream,deployment,file_end,data_start_file,'','NOT_AVAILABLE','','check: data gap between files',user)
+                #         file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+
+                # if there are no gaps in the file, identifies available data
                 if not gaps:
-                    if deploy_end is data_end:
-                        pass
-                    else:
-                        newline = (stream,deployment,data_end,deploy_end,'','NOT_AVAILABLE','','check - data end does not equal deployment end date',user)
-                        stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
-                else:
-                    cnt = 0
-                    for g in gaps:
-                        gap_start = g[0]
-                        gap_end = g[-1]
-                        if cnt is 0:
-                            newline = (stream,deployment,data_begin,gap_start,'','NOT_EVALUATED','','check - evaluate parameters',user)
-                            stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                    newline = (stream,deployment,data_start_file,data_end_file,'','NOT_EVALUATED','','check: evaluate parameters',user)
+                    file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                    file_end = data_end_file
+
+                    if cnf is len(file_list)-1: # last file, check against deployment end date from asset management
+                        if deploy_end is data_end_file:
+                            pass
                         else:
-                            newline = (stream,deployment,g1,gap_start,'','NOT_EVALUATED','','check - evaluate parameters',user)
-                            stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                            newline = (stream,deployment,data_end_file,deploy_end,'','NOT_AVAILABLE','','check: data end does not equal deployment end date',user)
+                            file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
-                        newline = (stream,deployment,gap_start,gap_end,'','NOT_AVAILABLE','','check - data gap',user)
-                        stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                # if there are gaps in the file, list the gap date ranges and available data date ranges
+                else:
+                    cnt = 0 # count of gaps
+                    for g in gaps:
+                        gap_start = g[0]  # gap start date
+                        gap_end = g[-1]   # gap end date
+                        if cnt is 0:
+                            newline = (stream,deployment,data_start_file,gap_start,'','NOT_EVALUATED','','check: evaluate parameters',user)
+                            file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                        else:
+                            newline = (stream,deployment,g1,gap_start,'','NOT_EVALUATED','','check: evaluate parameters',user)
+                            file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
-                        if cnt is len(gaps)-1:
-                            newline = (stream,deployment,gap_end,data_end,'','NOT_EVALUATED','','check - evaluate parameters',user)
-                            stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                        newline = (stream,deployment,gap_start,gap_end,'','NOT_AVAILABLE','','check: data gap',user)
+                        file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
-                            if deploy_end is data_end:
-                                pass
-                            else:
-                                newline = (stream,deployment,data_end,deploy_end,'','NOT_EVALUATED','','check - data end does not equal deployment end date',user)
-                                stream_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                        if cnt is len(gaps)-1: # last gap, prints available data between the end date of the last gap and the end date of data in the file
+                            newline = (stream,deployment,gap_end,data_end_file,'','NOT_EVALUATED','','check: evaluate parameters',user)
+                            file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+
+                            if cnf is len(file_list)-1: # last file, check against deployment end date from asset management
+                                if deploy_end is data_end_file:
+                                    pass
+                                else:
+                                    newline = (stream,deployment,data_end_file,deploy_end,'','NOT_AVAILABLE','','check: data end does not equal deployment end date',user)
+                                    file.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
                         g1 = gap_end
+                        file_end = data_end_file
                         cnt = cnt + 1
+
+                    cnf = cnf + 1
 
 
 def main(dataset, save_dir, user):
@@ -96,21 +149,25 @@ def main(dataset, save_dir, user):
     make_dir(drafts_dir)
 
     dm_stream = dataset.split('/')[-1].split('__')[1]
+    stream_name = dm_stream.split('-')[-1]
     stream_file = os.path.join(drafts_dir,dm_stream + '_processed_on_' + dt.now().strftime('%Y-%m-%dT%H%M%S') + '.csv')
+    stream_file_other = os.path.join(drafts_dir,'collocated_inst_streams_processed_on_' + dt.now().strftime('%Y-%m-%dT%H%M%S') + '.csv')
 
     with open(stream_file,'a') as stream_csv: # stream-level annotation .csv
-        if os.stat(stream_file).st_size==0:
-            writer = csv.writer(stream_csv)
-            writer.writerow(['Level', 'Deployment', 'StartTime', 'EndTime', 'Annotation', 'Status',
-                                       'Redmine#', 'Todo', 'reviewed_by'])
-            extract_gaps(data,stream_csv,user)
-        else:
-            print 'File already exists'
-            pass
+        writer = csv.writer(stream_csv)
+        writer.writerow(['Level', 'Deployment', 'StartTime', 'EndTime', 'Annotation', 'Status', 'Redmine#', 'Todo', 'reviewed_by'])
+        with open(stream_file_other,'a') as stream_csv_other: # stream-level annotation .csv
+            writer = csv.writer(stream_csv_other)
+            writer.writerow(['Level', 'Deployment', 'StartTime', 'EndTime', 'Annotation', 'Status', 'Redmine#', 'Todo', 'reviewed_by'])
+            extract_gaps(data, stream_csv, stream_csv_other, stream_name, user)
+
+    # delete the collocated_inst_streams file if file is empty
+    if os.stat(stream_file_other).st_size < 90:
+        os.remove(stream_file_other)
 
 
 if __name__ == '__main__':
-    dataset = '/Users/lgarzio/Documents/OOI/DataReviews/2017/RIC/CTDs/cabled/CE04OSBP-LJ01C-06-CTDBPO108/CE04OSBP-LJ01C-06-CTDBPO108__streamed-ctdbp_no_sample__requested.json'
-    annotations_dir = '/Users/lgarzio/Documents/repo/OOI/ooi-data-review/annotations/annotations'
+    dataset = '/Users/lgarzio/Documents/OOI/DataReviews/2017/RIC/test/CE07SHSM-RID26-04-VELPTA000__recovered_host-velpt_ab_dcl_instrument_recovered__requested-20170327T193853.json'
+    annotations_dir = '/Users/lgarzio/Documents/OOI/DataReviews/2017/RIC/test'
     user = 'lori'
     main(dataset, annotations_dir, user)
--- tools/annotate_variable.py
+++ tools/annotate_variable.py
@@ -23,57 +23,124 @@ def make_dir(save_dir):
     except OSError:
         pass
 
-def annotate_variable(data, parameter_csv, user):
+def annotate_variable(data, parameter_csv, parameter_issues_csv, stream_name, user):
     for d in data['deployments']:
         deployment = d
-        for s in data['deployments'][d]['streams']:
-            for x in data['deployments'][d]['streams'][s]['files']:
-                data_begin = data['deployments'][d]['streams'][s]['files'][x]['data_start']
-                data_end = data['deployments'][d]['streams'][s]['files'][x]['data_end']
-                vars = data['deployments'][d]['streams'][s]['files'][x]['variables']
-                misc = ['time']
-                reg_ex = re.compile('|'.join(misc))
-                sci_vars = [nn for nn in vars if not reg_ex.search(nn)]
+        s = stream_name
+        for x in data['deployments'][d]['streams'][s]['files']:
+            data_begin = data['deployments'][d]['streams'][s]['files'][x]['data_start']
+            data_end = data['deployments'][d]['streams'][s]['files'][x]['data_end']
+            vars_not_in_db = data['deployments'][d]['streams'][s]['files'][x]['vars_not_in_db']
+            vars_not_in_file = data['deployments'][d]['streams'][s]['files'][x]['vars_not_in_file']
 
-                for v in sci_vars:
-                    print v
-                    parameter = v
+            if not vars_not_in_db:
+                pass
+            else:
+                for i in vars_not_in_db:
+                    newline = (i, deployment, data_begin, data_end, '', 'vars_not_in_db', '', 'check: variable listed in file but not in database', user)
+                    parameter_issues_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
-                    t1 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['available']
-                    if intern(t1) is intern('True'):
+            if not vars_not_in_file:
+                pass
+            else:
+                for ii in vars_not_in_file:
+                    if ii == 'time':  # time will never be listed as a variable in the files
                         pass
                     else:
-                        print t1
-                        flag = 'NOT_AVAILABLE'
-                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested -' + t1, user)
+                        newline = (ii, deployment, data_begin, data_end, '', 'NOT_AVAILABLE', '', 'check: variable listed in database but not in file', user)
                         parameter_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
 
+            vars = data['deployments'][d]['streams'][s]['files'][x]['variables']
+            misc = ['time']
+            reg_ex = re.compile('|'.join(misc))
+            sci_vars = [nn for nn in vars if not reg_ex.search(nn)]
+
+            for v in sci_vars:
+                print v
+                parameter = v
+
+                t1 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['available']
+                if intern(t1) is intern('True'):
+                    pass
+                else:
+                    print t1
+                    flag = 'NOT_AVAILABLE'
+                    newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested availability: ' + t1, user)
+                    parameter_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+
+                try:
                     t2 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['all_nans']
                     if intern(t2) is intern('False'):
                         pass
                     else:
                         print t2
                         flag = 'FAILED'
-                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested -' + t2, user)
+                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested all_nans: ' + t2, user)
                         parameter_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
 
+                try:
                     t3 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['fill_test']
                     if intern(t3) is intern('False'):
                         pass
                     else:
                         print t3
                         flag = 'FAILED'
-                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested -' + t3, user)
+                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested fill_test: ' + t3, user)
                         parameter_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
 
+                try:
                     t4 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['fill_value']
                     if t4 == -9999999.0:
                         pass
                     else:
                         print t4
-                        flag = 'FAILED'
-                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested -' + str(-9999999.0) + 'found:' + str(t4) , user)
-                        parameter_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                        flag = 'Fill Value'
+                        newline = (parameter, deployment, data_begin, data_end, '', flag, '', 'tested: ' + str(-9999999.0) + ' found: ' + str(t4) , user)
+                        parameter_issues_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
+
+                try:
+                    global_max = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['global_max']
+                    global_min = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['global_min']
+                    newline = (parameter, deployment, data_begin, data_end, '', 'Global Range Values', '', 'global min = ' + str(global_min) + ' global_max = ' + str(global_max), user)
+                    parameter_issues_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
+
+                try:
+                    t5 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['global_range_test']
+                    if not t5:
+                        pass
+                    else:
+                        newline = (parameter, deployment, data_begin, data_end, '', 'Global Range QC Test', '', 'check: test triggered', user)
+                        parameter_issues_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
+
+                try:
+                    t6 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['dataqc_spiketest']
+                    if not t6:
+                        pass
+                    else:
+                        newline = (parameter, deployment, data_begin, data_end, '', 'Spike QC Test', '', 'check: test triggered', user)
+                        parameter_issues_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
+
+                try:
+                    t7 = data['deployments'][d]['streams'][s]['files'][x]['variables'][v]['dataqc_stuckvaluetest']
+                    if not t7:
+                        pass
+                    else:
+                        newline = (parameter, deployment, data_begin, data_end, '', 'Stuck Value QC Test', '', 'check: test triggered', user)
+                        parameter_issues_csv.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % newline)
+                except KeyError:
+                    pass
 
 
 def main(dataset, save_dir, user):
@@ -93,21 +160,26 @@ def main(dataset, save_dir, user):
     make_dir(drafts_dir)
 
     dm_stream = dataset.split('/')[-1].split('__')[1]
+    stream_name = dm_stream.split('-')[-1]
     parameter_file = os.path.join(drafts_dir,
                                dm_stream + '_parameters_processed_on_' + dt.now().strftime('%Y-%m-%dT%H%M%S') + '.csv')
+    parameter_issues = os.path.join(drafts_dir,
+                               dm_stream + '_parameter_issues_processed_on_' + dt.now().strftime('%Y-%m-%dT%H%M%S') + '.csv')
 
     with open(parameter_file, 'a') as parameter_csv:  # stream-level annotation .csv
-        if os.stat(parameter_file).st_size == 0:
-            writer = csv.writer(parameter_csv)
-            writer.writerow(['Level', 'Deployment', 'StartTime', 'EndTime', 'Annotation', 'Status',
-                             'Redmine#', 'Todo', 'reviewed_by'])
-            annotate_variable(data, parameter_csv, user)
-        else:
-            print 'File already exists'
-            pass
+        writer = csv.writer(parameter_csv)
+        writer.writerow(['Level', 'Deployment', 'StartTime', 'EndTime', 'Annotation', 'Status', 'Redmine#', 'Todo', 'reviewed_by'])
+        with open(parameter_issues, 'a') as parameter_issues_csv:  # stream-level annotation .csv
+            writer = csv.writer(parameter_issues_csv)
+            writer.writerow(['Level', 'Deployment', 'StartTime', 'EndTime', 'Annotation', 'Test', 'Redmine#', 'Todo', 'reviewed_by'])
+
+            annotate_variable(data, parameter_csv, parameter_issues_csv, stream_name, user)
+
 
 if __name__ == '__main__':
-    dataset = '/Users/leila/Documents/OOI_GitHub_repo/output_ric/CE04OSPS-SF01B-2A-CTDPFA107-streamed/test/GP03FLMA-RIM01-02-CTDMOG040__telemetered-ctdmo_ghqr_sio_mule_instrument__requested-20170315T142924.json'
-    annotations_dir = '/Users/leila/Documents/OOI_GitHub_repo/output_ric/CE04OSPS-SF01B-2A-CTDPFA107-streamed/test'
+#    dataset = '/Users/leila/Documents/OOI_GitHub_repo/output_ric/CE04OSPS-SF01B-2A-CTDPFA107-streamed/test/GP03FLMA-RIM01-02-CTDMOG040__telemetered-ctdmo_ghqr_sio_mule_instrument__requested-20170315T142924.json'
+#    annotations_dir = '/Users/leila/Documents/OOI_GitHub_repo/output_ric/CE04OSPS-SF01B-2A-CTDPFA107-streamed/test'
+    dataset = '/Users/lgarzio/Documents/OOI/DataReviews/2017/RIC/test/GI01SUMO-SBD11-06-METBKA000__recovered_host-metbk_a_dcl_instrument_recovered__requested-20170227T202508.json'
+    annotations_dir = '/Users/lgarzio/Documents/OOI/DataReviews/2017/RIC/test'
     user = 'leila'
     main(dataset, annotations_dir, user)
