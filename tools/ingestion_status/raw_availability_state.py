f7cf4a05e914eb0623569be0444aa6bdd64d9ce4
leilabbb <leila.ocean@gmail.com>
2017-07-27 10:38:49 -0400

    added script to compute statistics for raw data availability

---
+++ tools/ingestion_status/raw_availability_state.py
@@ -0,0 +1,99 @@
+#! /usr/local/bin/python
+
+"""
+Created on Mon July 25 2017
+@author: leilabbb
+"""
+from __future__ import division
+
+import pandas as pd
+import os
+import time
+
+
+start_time = time.time()
+
+'''
+This script recreate the ingestion and deployment sheets
+'''
+# select the platform
+platform = 'CP01CNSM'
+# path to baseline file
+rootdir = '/Users/leila/Documents/OOI_GitHub_repo/work/ingest-status/000_ingestpy_run_results/'
+# select the ingestion file example _D00003_ingest.csv or leave it as generic _ingest.csv
+key_file = '_P.csv'
+# headers' name for ingestion files
+col_header= ['Automated_status', 'data_source', 'deployment#', 'count', 'total', 'percent', 'note']
+
+# directory = os.path.dirname(rootdir+platform+'/')
+# if not os.path.exists(directory):
+#         os.makedirs(directory)
+
+# df = pd.DataFrame()
+for item in os.listdir(rootdir):
+    if item.startswith(platform):
+        if item.endswith(key_file):
+            if os.path.isfile(os.path.join(rootdir, item)):
+                print item
+                with open(os.path.join(rootdir, item), 'r') as csv_file:
+                    filereader = pd.read_csv(csv_file)
+                    status_list = list(pd.unique(filereader['Automated_status'].ravel()))
+                    deploy_list = list(pd.unique(filereader['deployment#'].ravel()))
+                    method_list = list(pd.unique(filereader['data_source'].ravel()))
+                    for methodx in method_list:
+                        #print methodx
+                        ind0 = filereader.loc[(filereader['data_source'] == methodx)]
+                        for deployx in deploy_list:
+                            #print deployx
+                            ind1 = ind0.loc[(ind0['deployment#'] == deployx)]
+                            total = len(ind1)
+                            counta = 0
+                            countm = 0
+                            for statusx in status_list:
+                                #print statusx
+
+                                ind2 = ind1.loc[(ind1['Automated_status'] == statusx)]
+                                count = len(ind2)
+
+                                if statusx == 'Not Deployed':
+                                    print 'Not Deployed found ', len(ind2)
+                                    counta += len(ind2)
+                                if statusx == 'Not Expected':
+                                    print 'Not Expected found ', len(ind2)
+                                    counta += len(ind2)
+                                if statusx == 'Pending':
+                                    print 'Pending found ', len(ind2)
+                                    counta += len(ind2)
+                                if statusx == 'Available':
+                                    print 'Available found ', len(ind2)
+                                    counta += len(ind2)
+
+                                if statusx == 'Missing':
+                                    print 'Missing found ', len(ind2)
+                                    countm = len(ind2)
+
+                                if total != 0:
+                                    percent = (count/total) * 100
+                                    note = ''
+                                else:
+                                    note = 'integer division or module by zero'
+                                    percent = total
+                                # print methodx, '...', deployx, '...', statusx, '...', count, '...', percent, '%', '....', note
+
+
+                            if total != 0:
+                                percenta = (counta/total) * 100
+                                percentm = (countm/total) * 100
+                                note = ''
+                            else:
+                                note = 'integer division or module by zero'
+                                percenta = total
+                                percentm = total
+
+                            print methodx, '...', deployx, 'Available', counta, '...', percenta, '%', '....', note
+                            print methodx, '...', deployx, 'Missing', countm, '...', percentm, '%', '....', note
+
+                    outputfile = rootdir + platform + '/' + platform + 'availability_state.csv'
+                    print outputfile
+
+                    ind2.to_csv(outputfile, index=False, columns=col_header, na_rep='', encoding='utf-8')
